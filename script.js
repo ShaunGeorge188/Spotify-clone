baf93c78553e156ff623b21a42137ec6d788563b
Shaun George <shaungeorge2006@gmail.com>
2026-08-22 23:23:02 +0530

    fix: styles, add songs to Library

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            let decoded = decodeURIComponent(element.href);
+            let filename = decoded.split(/[\\/]/).pop();
+            songs.push(filename);
         }
 
     }
@@ -24,6 +26,11 @@ async function getSongs() {
 async function main() {
     let songs = await getSongs();
     console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for(const song of songs){
+        songUL.innerHTML = songUL.innerHTML +  `<li> ${song.replaceAll("%20", " ")} </li>`
+    }
 
     var audio = new Audio(songs[0]);
     //audio.play();
