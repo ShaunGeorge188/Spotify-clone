66dc5bda7e95fad0c33bfc25d942116bdca62f4e
Shaun George <shaungeorge2006@gmail.com>
2026-08-23 15:02:34 +0530

    update: add songThumbnail, song and artist names, hover play button on thumbnail

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Let's write javascript");
 
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let a = await fetch("http://127.0.0.1:3002/songs/");
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -31,7 +31,25 @@ async function main() {
     songUL.innerHTML = songs
         .map(song => {
             let name = song.replaceAll("%20", " ").replace(".mp3", "");
-            return `<li title="${name}">${name}</li>`;
+            let parts = name.split("-");
+            let artist = parts[0];
+            let title = parts.slice(1).join(" ").trim();
+
+            return `
+            <li title="${name}">
+                
+                <div class="thumbWrapper">
+                    <img class="songThumb" src="music.svg" alt="">
+                    <svg class="playOverlay" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="white">
+                        <path d="M8 5v14l11-7z"></path>
+                    </svg>
+                </div>
+                <div class="songText">
+                    <span class="songTitle">${title}</span>
+                    <span class="songArtist">${artist}</span>
+                </div>
+            </li>
+        `;
         })
         .join("");
 
