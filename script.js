cfc5f4e22a770d2a9c56b95dde6d23da9ae15438
Shaun George <shaungeorge2006@gmail.com>
2026-08-27 16:55:31 +0530

    fix: fetch songs from static json instead of local server

--- script.js
+++ script.js
@@ -22,22 +22,8 @@ function formatTime(seconds) {
 }
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/songs/");
-    let response = await a.text();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    let songList = [];
-
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            let decoded = decodeURIComponent(element.href);
-            let filename = decoded.split(/[\\/]/).pop();
-            songList.push(filename);
-        }
-    }
-    return songList;
+    let res = await fetch("songs.json");
+    return await res.json();
 }
 
 async function getAlbums() {
